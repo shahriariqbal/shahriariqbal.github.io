dc12da50fcd8fdf4f3db8617d1b48a2325a0a897
shahriariqbal <shahriariqbal@users.noreply.github.com>
2026-09-11 20:04:35 +0300

    fix: real footer data; About updated to 4+ years + correct degree name

--- components/footer.js
+++ components/footer.js
@@ -4,14 +4,17 @@ class Footer extends HTMLElement {
   }
 
   connectedCallback() {
+    const startYear = 2024;
+    const currentYear = new Date().getFullYear();
+    const yearDisplay = startYear === currentYear ? `${startYear}` : `${startYear}–${currentYear}`;
     this.innerHTML = `
     <div class="footer-lemos mt-5">
 
         <div class="py-3 px-md-5 container-fluid">
             <div class="row">
                 <div class="col-auto col-md-5 align-middle">
-                    <p class="mb-0" style="color: var(--light-gray);">© 2024 Your Name</p>
-                    <p class="my-0 pt-0"><a class="" href="mailto:yourname@email.org">yourname@email.org</a></p>
+                    <p class="mb-0" style="color: var(--light-gray);">© ${yearDisplay} Shahriar Iqbal</p>
+                    <p class="my-0 pt-0"><a class="" href="mailto:shahriar.iqbal.dev@gmail.com">shahriar.iqbal.dev@gmail.com</a></p>
 
                 </div>
 
@@ -19,11 +22,10 @@ class Footer extends HTMLElement {
                 <div class="col col-md-7 text-right">
                     <p class="mb-0" style="color: var(--light-gray);">Quick Links</p>
                     <p class="my-0 pt-0">
-                        <a href=""><i class="mr-3 footer-icons fa-solid fa-house" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 fab footer-icons fa-linkedin" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 footer-icons fa-brands fa-google-scholar" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 fab footer-icons fa-github" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="footer-icons fa-brands fa-x-twitter" aria-hidden="true"></i></a>
+                        <a href="/"><i class="mr-3 footer-icons fa-solid fa-house" aria-hidden="true"></i></a>
+                        <a href="https://linkedin.com/in/shahriar-iqbal" target="_blank"><i class="mr-3 fab footer-icons fa-linkedin" aria-hidden="true"></i></a>
+                        <a href="https://github.com/shahriariqbal" target="_blank"><i class="mr-3 fab footer-icons fa-github" aria-hidden="true"></i></a>
+                        <a href="https://shahriariqbal.com/XRMeditation/" target="_blank"><i class="footer-icons fa-solid fa-vr-cardboard" aria-hidden="true" title="XR Meditation"></i></a>
                     </p>
                 </div>
             </div>
